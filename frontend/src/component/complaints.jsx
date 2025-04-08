9a8a3526762887b8ab841223e6522c3feb986961
Dazzler <catc.daz07@gmail.com>
2025-04-08 09:20:01 +0800

    Update complaints.jsx

--- frontend/src/component/complaints.jsx
+++ frontend/src/component/complaints.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
 function Complain() {
   return (
-    <>
-      <h2>Complain Page</h2>
-    </>
+    <div className='w-full h-screen flex justify-center items-center'>
+      <h2 className='text-[20pt] w-full h-[10%] '>Complain Page</h2>
+      <div className=''>
+        
+    </div>
+    </div>
   );
 }
 
